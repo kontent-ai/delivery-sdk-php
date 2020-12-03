fb72377445bce017815dd3fbc9682f43fd76547d
ondrejch <ondrejch@kentico.com>
2020-12-03 15:31:35 +0100

    polish assertions

--- tests/Unit/UrlBuilderTest.php
+++ tests/Unit/UrlBuilderTest.php
@@ -48,9 +48,12 @@ class UrlBuilderTest extends TestCase
         $params = (new QueryParams())
         ->skip(2)
         ->orderAsc('title')
+
         ->all('elements.personas', array('barista', 'coffee_blogger'))
         ->any('elements.description', 'Hello')
         ->contains('elements.title', 'a')
+        ->in('elements.margin', array(21,15,19))
+        ->notIn('elements.oldMargin', array(42, 666))
 
         ->equals('elements.isfeatured', 'true')
         ->notEquals('system.type', 'article')
@@ -62,18 +65,18 @@ class UrlBuilderTest extends TestCase
         ->greaterThanOrEqual('elements.oldprice', 7)
         ->range('elements.shoesize', 7, 9)
         ->lessThan('elements.tax', 21)
-        ->lessThanOrEqual('elements.oldtax', 25)
-
-        ->in('elements.margin', array(21,15,19))
-        ->notIn('elements.oldMargin', array(42, 666));
+        ->lessThanOrEqual('elements.oldtax', 25);
 
         $url = $builder->getItemsUrl($params);
 
         $this->assertContains('skip=2', $url);
         $this->assertContains('order=title%5Basc%5D', $url);
+
         $this->assertContains('elements.personas%5Ball%5D=barista%2Ccoffee_blogger', $url);
         $this->assertContains('elements.description%5Bany%5D=Hello', $url);
         $this->assertContains('elements.title%5Bcontains%5D=a', $url);
+        $this->assertContains('elements.margin%5Bin%5D=21%2C15%2C19', $url);
+        $this->assertContains('elements.oldMargin%5Bnin%5D=42%2C666', $url);
 
         $this->assertContains('elements.isfeatured=true', $url);
         $this->assertContains('system.type%5Bneq%5D=article', $url);
@@ -86,8 +89,5 @@ class UrlBuilderTest extends TestCase
         $this->assertContains('elements.shoesize%5Brange%5D=7%2C9', $url);
         $this->assertContains('elements.tax%5Blt%5D=21', $url);
         $this->assertContains('elements.oldtax%5Blte%5D=25', $url);
-
-        $this->assertContains('elements.margin%5Bin%5D=21%2C15%2C19', $url);
-        $this->assertContains('elements.oldMargin%5Bnin%5D=42%2C666', $url);
     }
 }
